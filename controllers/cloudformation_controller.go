8f80cccf6004789fd03d55f440f9c085349766de
Srini Maddineni <maddinenisri@gmail.com>
2021-02-02 20:17:42 -0500

    Generated rbac

--- controllers/cloudformation_controller.go
+++ controllers/cloudformation_controller.go
@@ -34,10 +34,6 @@ type CloudFormationReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=infra.mdstechinc.com,resources=cloudformations,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=infra.mdstechinc.com,resources=cloudformations/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=infra.mdstechinc.com,resources=cloudformations/finalizers,verbs=update
-
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
@@ -47,10 +43,13 @@ type CloudFormationReconciler struct {
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
-func (r *CloudFormationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = r.Log.WithValues("cloudformation", req.NamespacedName)
 
-	// your logic here
+// +kubebuilder:rbac:groups=infra.mdstechinc.com,resources=cloudformations,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=infra.mdstechinc.com,resources=cloudformations/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=infra.mdstechinc.com,resources=cloudformations/finalizers,verbs=update
+func (r *CloudFormationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := r.Log.WithValues("cloudformation", req.NamespacedName)
+	log.Info("Reconciling CloudFormation CRD")
 
 	return ctrl.Result{}, nil
 }
